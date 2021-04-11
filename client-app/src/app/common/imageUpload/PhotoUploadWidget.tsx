117f2efeda59531729c9a3443894346fa7361754
aandoney <angel_andoney@live.com.mx>
2021-04-11 12:11:08 -0500

    add client side image profile

---
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { Button, Grid, Header } from "semantic-ui-react";
+import { PhotoFile } from "../../models/Profile";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
+import PhotoWidgetDropZone from "./PhotoWidgetDropzone";
+
+interface Props {
+  loading: boolean;
+  uploadPhoto: (file: Blob) => void;
+}
+
+export default function PhotoUploadWidget({ loading, uploadPhoto }: Props) {
+  const [files, setFiles] = useState<PhotoFile[]>([]);
+  const [cropper, setCropper] = useState<Cropper>();
+
+  function onCrop() {
+    if (cropper) {
+      cropper.getCroppedCanvas().toBlob((blob) => uploadPhoto(blob!));
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      files.forEach((file: PhotoFile) => URL.revokeObjectURL(file.preview));
+    };
+  }, [files]);
+
+  return (
+    <Grid>
+      <Grid.Column width={4}>
+        <Header sub color="teal" content="Step 1 - Add Photo" />
+        <PhotoWidgetDropZone setFiles={setFiles} />
+      </Grid.Column>
+      <Grid.Column width={1} />
+      <Grid.Column width={4}>
+        <Header sub color="teal" content="Step 2 - Resize image" />
+        {files && files.length > 0 && (
+          <PhotoWidgetCropper
+            setCropper={setCropper}
+            imagePreview={files[0].preview}
+          />
+        )}
+      </Grid.Column>
+      <Grid.Column width={1} />
+      <Grid.Column width={4}>
+        <Header sub color="teal" content="Step 3 - Preview & upload" />
+        {files && files.length > 0 && (
+          <>
+            <div
+              className="img-preview"
+              style={{ minHeight: 200, overflow: "hidden" }}
+            />
+            <Button.Group widths={2}>
+              <Button
+                loading={loading}
+                onClick={onCrop}
+                positive
+                icon="check"
+              />
+              <Button
+                disabled={loading}
+                onClick={() => setFiles([])}
+                icon="close"
+              />
+            </Button.Group>
+          </>
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+}
